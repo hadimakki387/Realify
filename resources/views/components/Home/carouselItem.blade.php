f7118b11aca738fc96b54313be9e42e472dde63f
hadimakki387 <hmakki387@gmail.com>
2023-05-22 00:25:46 +0300

    made the carousel reusable

--- resources/views/components/Home/carouselItem.blade.php
+++ resources/views/components/Home/carouselItem.blade.php
@@ -1,4 +1,5 @@
-<div class="w-full flex-none">
+
+    <div class="w-full flex-none">
     <div class="flex gap-2">
       <div class="grid grid-cols-9 grid-rows-4 gap-3 w-[50%] h-[400px]">
         <div class="col-span-3 row-span-4 relative overflow-hidden rounded-lg">
@@ -38,7 +39,6 @@
             <div class="h-full w-full bg-[url('house-03.jpg')] bg-cover bg-center transition-transform duration-700 transform-gpu scale-100 hover:scale-110"></div>
         </div>
     </div>
-    
-    
     </div>
   </div>
+
